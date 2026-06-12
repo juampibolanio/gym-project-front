f567a9347f9a8046c51251685c20657ff16a9ad0
JuampiBolanio <juampi360786@gmail.com>
2026-06-12 15:27:27 -0300

    feat(plans): agregar interfaces y esquemas para la gestión de planes

---
+++ src/features/plans/interfaces/plan.interface.ts
@@ -0,0 +1,16 @@
+export interface Plan {
+    uuid: string;
+    name: string;
+    price: number;
+    durationDays: number;
+    description?: string;
+}
+
+export interface CreatePlanPayload {
+    name: string;
+    price: number;
+    durationDays: number;
+    description?: string;
+}
+
+export type UpdatePlanPayload = Partial<CreatePlanPayload>;
