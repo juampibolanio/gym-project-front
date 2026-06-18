ffc305a2227314eeb04b3979b085d9f73aa5fc95
JuampiBolanio <juampi360786@gmail.com>
2026-06-18 14:56:52 -0300

    feat(gyms): agregar funcionalidad para actualizar información de gimnasio

--- src/features/gyms/services/gyms.service.ts
+++ src/features/gyms/services/gyms.service.ts
@@ -1,5 +1,5 @@
 import { httpClient } from "@/core/api/axios.adapter";
-import { Gym } from "../interfaces/gym.interface";
+import { Gym, UpdateGymPayload } from "../interfaces/gym.interface";
 
 export class GymsService {
     private static readonly ENDPOINT = '/gyms';
@@ -8,5 +8,10 @@ export class GymsService {
         console.log(`term now: ${term}`)
         return await httpClient.get<Gym>(`${this.ENDPOINT}/${term}`);
     }
+
+    static async update(id: string, payload: UpdateGymPayload): Promise<Gym> {
+        return await httpClient.patch<Gym>(`${this.ENDPOINT}/${id}`, payload);
+    }
+
     // todo: agregar el resto de metodos cuando se haga el dashboard para los superadmin
 }
