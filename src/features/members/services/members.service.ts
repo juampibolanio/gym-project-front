3336ddc740bbd36d4e986541954ea9de8284d02a
Manuel0803 <manulautaro2003@gmail.com>
2026-06-18 20:32:29 -0300

    feature(member): crear service miembros

---
+++ src/features/members/services/members.service.ts
@@ -0,0 +1,27 @@
+import { PaginatedResult } from "@/common/interfaces/pagination.interface";
+import { httpClient } from "@/core/api/axios.adapter";
+import { CreateMemberPayload, Member, UpdateMemberPayload } from "../interfaces/members.interface";
+
+export class MembersService {
+    private static readonly ENDPOINT = '/members';
+
+    static async getAll(page: number = 1, limit: number = 10): Promise<PaginatedResult<Member>> {
+        return await httpClient.get<PaginatedResult<Member>>(`${this.ENDPOINT}?page=${page}&limit=${limit}`);
+    }
+
+    static async getById(id: string): Promise<Member> {
+        return await httpClient.get<Member>(`${this.ENDPOINT}/${id}`);
+    }
+
+    static async create(payload: CreateMemberPayload): Promise<Member> {
+        return await httpClient.post(this.ENDPOINT, payload);
+    }
+
+    static async update(id: string, payload: UpdateMemberPayload): Promise<Member> {
+        return await httpClient.patch<Member>(`${this.ENDPOINT}/${id}`, payload);
+    }
+
+    static async remove(id: string): Promise<Member> {
+        return await httpClient.delete<Member>(`${this.ENDPOINT}/${id}`);
+    }
+}
