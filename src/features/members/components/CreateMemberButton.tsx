5f060823e39e300b68074b275db519530cfa6a46
Manuel0803 <manulautaro2003@gmail.com>
2026-06-19 17:15:59 -0300

    feat(visuals): cambios visuales en el css responsive

--- src/features/members/components/CreateMemberButton.tsx
+++ src/features/members/components/CreateMemberButton.tsx
@@ -11,7 +11,7 @@ export default function CreateMemberButton() {
       {isAdmin && (
         <Link
           href="/dashboard/miembros/nuevo"
-          className="bg-brand-main hover:bg-brand-hover text-white px-4 py-2 rounded-lg font-medium text-sm transition-colors "
+          className="bg-brand-main hover:bg-brand-hover text-white px-4 py-2 rounded-lg font-medium text-sm transition-colors text-center"
         >
           + Nuevo Miembro
         </Link>
