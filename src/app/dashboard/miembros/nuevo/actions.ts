c427308c0ca18875bec4aa543f3cc339242fc7b5
JuampiBolanio <juampi360786@gmail.com>
2026-07-14 14:17:19 -0300

    fix(gyms): eliminar console log en desuso

--- src/app/dashboard/miembros/nuevo/actions.ts
+++ src/app/dashboard/miembros/nuevo/actions.ts
@@ -8,10 +8,8 @@ export async function createMemberAction(
   try {
     const validData = createMemberSchema.parse(data);
 
-    console.log('Datos validados y listos para BD:', validData);
     return { success: true, message: 'Miembro registrado exitosamente' };
   } catch (error) {
-    console.error('Error de validación en el servidor:', error);
     return { success: false, error: 'Datos inválidos' };
   }
 }
